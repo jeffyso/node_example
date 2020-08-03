ec466d60f78c219fa4dff02a08bf0ceb92e69852
jeffyso <jeffyzaja@gmail.com>
2020-08-03 15:05:37 +0700

    Initial Project

---
+++ index.js
@@ -0,0 +1,16 @@
+// const arr = [1,2,3,4]
+// const arr2 =[...arr]
+// const arr3 =[...arr,...arr2]
+
+// arr2[2] =50;
+// console.log(arr2);
+
+// const obj = {num:10,num2:20};
+// const obj2 = {...obj}
+
+// console.log(obj2);0
+
+
+const multiply5 = (num = 5) => num*5
+
+console.log(multiply5());
